011ede1e6ea38c6067386d7fedbbb730b085cc2d
Chaim Shlez <chaimshlez@gmail.com>
2025-08-10 17:31:02 +0300

    test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import mission_processor
-from mission_processor import load_mission_data, filter_by_status,count_by_priority
+from mission_processor import load_mission_data, filter_by_status
 from personnel_analyzer import load_personnel_data, filter_by_clearance,group_by_unit
 from report_generator import generate_mission_summary,generate_personnel_report
+
+
 
 
 
